67d76a7f874458da2ee27dfe9894513d8a7c9e15
Raphael Saldanha <rfsaldanha@gmail.com>
2022-06-23 12:11:31 -0300

    Example indicator with parallel computing.

---
+++ R/indi_0001.R
@@ -0,0 +1,56 @@
+#' Indicator: Mortalidade por causas externas
+#'
+#' @param agg character. Spatial aggregation level. \code{uf_res} for UF of residence. \code{uf_ocor} for UF of occurrence. \code{regsaude_res} for regiao de saude of residence. \code{regsaude_ocor} for regiao de saúde of occurence. \code{mun_res} for municipality of residence. \code{mun_ocor} for municipality of ocurrence.
+#' @param ano numeric. Year of death.
+#' @param decimals integer. Number of decimals for indicator.
+#' @param pcdas_token character. PCDaS API token. If not provided, the function will look for it on renvirom.
+#'
+#' @examples
+#' # Some examples
+#' indi_0001(agg = "mun_res", ano = 2013)
+#'
+#' @importFrom rlang .data
+#' @importFrom foreach %dopar%
+#' @export
+indi_0001 <- function(agg, ano, decimals = 2, pcdas_token = NULL){
+
+  # Try to get PCDaS API token from renviron if not provided
+  if(is.null(pcdas_token)){
+    pcdas_token <- get_pcdas_token_renviron()
+  }
+
+  # Creates numerator
+  if(length(ano) == 1){
+    numerador <- get_sim(
+      agg = agg, ano = ano,
+      pcdas_token = pcdas_token,
+      more_filters = "LEFT(CAUSABAS, 1) IN ('V', 'W', 'X', 'Y')"
+    )
+  } else if(length(ano) > 1){
+    # Start parallel cluster
+    doParallel::registerDoParallel(parallel::detectCores() - 1)
+
+    # Get numerator in parallel
+    a <- NULL
+    numerador <- foreach::foreach(a = ano, .combine = dplyr::bind_rows) %dopar% {
+      get_sim(agg = agg, ano = a, more_filters = "LEFT(CAUSABAS, 1) IN ('V', 'W', 'X', 'Y')")
+    }
+
+    # Stop cluster
+    doParallel::stopImplicitCluster()
+  }
+
+  # Creates denominator
+  if(agg == "mun_res"){
+    denominador <- bilis::mun_pop
+  }
+
+  # Join numerator and denominator, peform indicator calculus
+  df <- dplyr::inner_join(x = numerador, y = denominador, by = c("agg" = "cod", "ano" = "ano")) %>%
+    dplyr::mutate(indi = round(
+      x = .data$freq/.data$pop * 1000,
+      digits = decimals
+    ))
+
+  return(df)
+}
