d838dd6049290b9a06a6991e0d614188389a0d7a
Raphael Saldanha <rfsaldanha@gmail.com>
2022-06-28 16:02:34 -0300

    Change formatting.

--- R/indi_0002.R
+++ R/indi_0002.R
@@ -34,7 +34,11 @@ indi_0002 <- function(agg, ano, multi = 100000, decimals = 2, pcdas_token = NULL
     # Get numerator in parallel
     a <- NULL
     numerador <- foreach::foreach(a = ano, .combine = dplyr::bind_rows) %dopar% {
-      get_sim(agg = agg, ano = a, pcdas_token = pcdas_token, cid_like = "I6")
+      get_sim(
+        agg = agg, ano = a,
+        pcdas_token = pcdas_token,
+        cid_like = "I6"
+      )
     }
 
     # Stop cluster
