e47a69b6a4f8294fe55c49a37880396a3a140a90
Raphael Saldanha <rfsaldanha@gmail.com>
2022-07-25 15:19:41 -0300

    Update namespace, documentation and tests without functions that were moved to rpcdas package.

--- tests/testthat/test-generic_pcdas_query.R
+++
@@ -1,5 +0,0 @@
-test_that("generic pcdas query works", {
-  res <- generic_pcdas_query(sql_query = "SELECT N_AIH, DT_INTER, PROC_REA FROM \"datasus-sih\" LIMIT 100")
-
-  expect_equal(nrow(res), 100)
-})
--- tests/testthat/test-list_pcdas_tables.R
+++
@@ -1,5 +0,0 @@
-test_that("list pcdas tables works", {
-  res <- list_pcdas_tables()
-
-  expect_equal("character", class(res))
-})
