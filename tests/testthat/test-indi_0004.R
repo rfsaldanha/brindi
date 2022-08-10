83fca8dd66e70a447b71eac59073545bb7a7cb22
Raphael Saldanha <rfsaldanha@gmail.com>
2022-08-10 15:23:51 -0300

    New tests.

---
+++ tests/testthat/test-indi_0004.R
@@ -0,0 +1,42 @@
+test_that("indi_0004 works with mun res", {
+  res <- indi_0004(agg = "mun_res", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 5000)
+})
+
+test_that("indi_0004 works with mun ocor", {
+  res <- indi_0004(agg = "mun_ocor", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 5000)
+})
+
+test_that("indi_0004 works with uf res", {
+  res <- indi_0004(agg = "uf_res", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 20)
+})
+
+test_that("indi_0004 works with uf ocor", {
+  res <- indi_0004(agg = "uf_ocor", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 20)
+})
+
+test_that("indi_0004 works with regsaude res", {
+  res <- indi_0004(agg = "regsaude_res", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 20)
+})
+
+test_that("indi_0004 works with regsaude ocor", {
+  res <- indi_0004(agg = "regsaude_res", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 20)
+})
+
