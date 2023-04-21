81a836853b41bf7ab7d1cb8644c71d9d143eeb0a
Raphael Saldanha <rfsaldanha@gmail.com>
2023-04-21 15:53:56 +0200

    indi_0004 tests correction

--- tests/testthat/test-indi_0004.R
+++ tests/testthat/test-indi_0004.R
@@ -2,14 +2,14 @@ test_that("indi_0004 works with mun res", {
   res <- indi_0004(agg = "mun_res", ano = 2013)
 
   expect_equal("tbl_df", class(res)[1])
-  expect_gt(nrow(res), 5000)
+  expect_gt(nrow(res), 4000)
 })
 
 test_that("indi_0004 works with mun ocor", {
   res <- indi_0004(agg = "mun_ocor", ano = 2013)
 
   expect_equal("tbl_df", class(res)[1])
-  expect_gt(nrow(res), 5000)
+  expect_gt(nrow(res), 4000)
 })
 
 test_that("indi_0004 works with uf res", {
@@ -34,7 +34,7 @@ test_that("indi_0004 works with regsaude res", {
 })
 
 test_that("indi_0004 works with regsaude ocor", {
-  res <- indi_0004(agg = "regsaude_res", ano = 2013)
+  res <- indi_0004(agg = "regsaude_ocor", ano = 2013)
 
   expect_equal("tbl_df", class(res)[1])
   expect_gt(nrow(res), 20)
