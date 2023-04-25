8c4e90f19f14e19079ec300ea151c32d46e1c3f9
Raphael Saldanha <rfsaldanha@gmail.com>
2023-04-25 11:05:45 +0200

    Update test parameters.

--- tests/testthat/test-indi_0002.R
+++ tests/testthat/test-indi_0002.R
@@ -40,3 +40,30 @@ test_that("indi_0002 works with regsaude ocor", {
   expect_gt(nrow(res), 20)
 })
 
+test_that("indi_0002 works with uf and month aggregation res", {
+  res <- indi_0002(agg = "uf_res", agg_time = "month", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 300)
+})
+
+test_that("indi_0002 works with uf and week aggregation res", {
+  res <- indi_0002(agg = "uf_res", agg_time = "week", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 1300)
+})
+
+test_that("indi_0002 works with mun_res and multiple years res", {
+  res <- indi_0002(agg = "mun_res", ano = c(2013, 2014))
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 10400)
+})
+
+test_that("indi_0002 works with uf_res and multiple years res", {
+  res <- indi_0002(agg = "uf_res", ano = c(2013, 2014))
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 50)
+})
