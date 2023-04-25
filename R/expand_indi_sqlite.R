38d4f0fbdb9f9593ca2a7b75c089149450e88f65
Raphael Saldanha <rfsaldanha@gmail.com>
2023-04-25 15:59:59 +0200

    Add agg_time argument.

--- R/expand_indi_sqlite.R
+++ R/expand_indi_sqlite.R
@@ -1,13 +1,14 @@
 #' Expand indicator results to SQLite
 #'
 #' @param agg character vector. Spatial aggregation level. \code{uf_res} for UF of residence. \code{uf_ocor} for UF of occurrence. \code{regsaude_res} for regiao de saude of residence. \code{regsaude_ocor} for regiao de saúde of occurence. \code{mun_res} for municipality of residence. \code{mun_ocor} for municipality of ocurrence.
+#' @param agg_time character vector. Time aggregation levels. \code{year} for yearly data. \code{month} for monthly data. \code{week} for weekly data. Defaults to \code{year}.
 #' @param anos numeric vector. Years
 #' @param db character. SQLite file address and name.
 #' @param indi character function names vector. Defaults to `all` for all indi_ functions.
 #' @param table_name character. Indicators table name.
 #'
 #' @export
-expand_indi_sqlite <- function(agg, anos, db, indi = "all", table_name = "indi"){
+expand_indi_sqlite <- function(agg, agg_time, anos, db, indi = "all", table_name = "indi"){
 
   # List indi_ functions or use supplied vector
   if(length(indi) == 1){
@@ -39,7 +40,7 @@ expand_indi_sqlite <- function(agg, anos, db, indi = "all", table_name = "indi")
   # Expand indi_functions and write to table
   for(i in indi_funs){
     pb$tick(tokens = list(what = i))
-    tmp <- expand_indi(agg = agg, anos = anos, indi_fun = i)
+    tmp <- expand_indi(agg = agg, agg_time = agg_time, anos = anos, indi_fun = i)
     DBI::dbWriteTable(conn = conn, name = table_name, value = tmp, append = TRUE)
   }
 
