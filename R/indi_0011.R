533502978d2e46d65cedbda67b20fa425664b5fe
Raphael Saldanha <rfsaldanha@gmail.com>
2023-04-25 12:05:21 +0200

    Multiple years query, month and week aggregation and tests.

--- R/indi_0011.R
+++ R/indi_0011.R
@@ -1,6 +1,7 @@
 #' Indicator: Taxa de Mortalidade em menores de 5 anos
 #'
 #' @param agg character. Spatial aggregation level. \code{uf_res} for UF of residence. \code{uf_ocor} for UF of occurrence. \code{regsaude_res} for regiao de saude of residence. \code{regsaude_ocor} for regiao de saúde of occurence. \code{mun_res} for municipality of residence. \code{mun_ocor} for municipality of ocurrence.
+#' @param agg_time character. Time aggregation level. \code{year} for yearly data. \code{month} for monthly data. \code{week} for weekly data. Defaults to \code{year}.
 #' @param ano numeric. Year of death.
 #' @param multi integer. Multiplicator for indicator.
 #' @param decimals integer. Number of decimals for indicator.
@@ -12,7 +13,7 @@
 #'
 #' @importFrom rlang .data
 #' @export
-indi_0011 <- function(agg, ano, multi = 1000, decimals = 2, pcdas_token = NULL){
+indi_0011 <- function(agg, agg_time = "year", ano, multi = 1000, decimals = 2, pcdas_token = NULL){
 
   # Try to get PCDaS API token from renviron if not provided
   if(is.null(pcdas_token)){
@@ -22,6 +23,7 @@ indi_0011 <- function(agg, ano, multi = 1000, decimals = 2, pcdas_token = NULL){
   # Creates numerator
   numerador <- rpcdas::get_sim(
     agg = agg,
+    agg_time = agg_time,
     ano = ano,
     idade_a = 0,
     idade_b = 4,
@@ -31,15 +33,16 @@ indi_0011 <- function(agg, ano, multi = 1000, decimals = 2, pcdas_token = NULL){
   # Creates denominator
   denominador <- rpcdas::get_sinasc(
     agg = agg,
+    agg_time = agg_time,
     ano = ano,
     pcdas_token = pcdas_token
-  )%>%
-    dplyr::rename(year = .data$ano, pop = .data$freq)
+  )
 
   # Perform indicator calculus
   res <- indicator_raw(
     numerador = numerador,
     denominador = denominador,
+    denominador_type = "arbitrary",
     multi = multi,
     decimals = decimals,
     nome = "indi_0011",
