b1aa119137e6261e78b08036bab68a85f1650373
Raphael Saldanha <rfsaldanha@gmail.com>
2023-04-25 12:10:44 +0200

    Multiple years query, month and week aggregation and tests.

---
+++ tests/testthat/test-indi_0012.R
@@ -0,0 +1,62 @@
+test_that("indi_0012 works with mun res", {
+  res <- indi_0012(agg = "mun_res", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 20)
+})
+
+test_that("indi_0012 works with mun ocor", {
+  res <- indi_0012(agg = "mun_ocor", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 10)
+})
+
+test_that("indi_0012 works with uf res", {
+  res <- indi_0012(agg = "uf_res", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 10)
+})
+
+test_that("indi_0012 works with uf ocor", {
+  res <- indi_0012(agg = "uf_ocor", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 5)
+})
+
+test_that("indi_0012 works with regsaude res", {
+  res <- indi_0012(agg = "regsaude_res", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 15)
+})
+
+test_that("indi_0012 works with regsaude ocor", {
+  res <- indi_0012(agg = "regsaude_ocor", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 15)
+})
+
+test_that("indi_0012 works with uf res and month aggregation", {
+  res <- indi_0012(agg = "uf_res", agg_time = "month", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 20)
+})
+
+test_that("indi_0012 works with uf res and week aggregation", {
+  res <- indi_0012(agg = "uf_res", agg_time = "week", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 20)
+})
+
+test_that("indi_0012 works with uf res and multiple years", {
+  res <- indi_0012(agg = "uf_res", ano = c(2013, 2015))
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 20)
+})
