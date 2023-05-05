6afcb5843a66cc5d452a51fa2b01c9008bc85db6
Raphael Saldanha <rfsaldanha@gmail.com>
2023-05-05 10:12:32 +0200

    Expand indicators to parquet files.

---
+++ R/expand_indi_parquet.R
@@ -0,0 +1,37 @@
+#' Expand several indicators results to SQLite
+#'
+#' @param agg character vector. Spatial aggregation level. \code{uf_res} for UF of residence. \code{uf_ocor} for UF of occurrence. \code{regsaude_res} for regiao de saude of residence. \code{regsaude_ocor} for regiao de saúde of occurence. \code{mun_res} for municipality of residence. \code{mun_ocor} for municipality of ocurrence.
+#' @param agg_time character vector. Time aggregation levels. \code{year} for yearly data. \code{month} for monthly data. \code{week} for weekly data. Defaults to \code{year}.
+#' @param anos numeric vector. Years
+#' @param dir character. Directory to save parquet files.
+#' @param indi character function names vector. Defaults to `all` for all indi_ functions.
+#'
+#' @export
+expand_indi_parquet <- function(agg, agg_time, anos, dir, indi = "all", table_name = "indi"){
+
+  # List indi_ functions or use supplied vector
+  if(length(indi) == 1){
+    if(indi == "all"){
+      indi_funs <- grep("^indi_", ls(getNamespace("bilis")), value = TRUE)
+    } else {
+      indi_funs <- c(indi)
+    }
+  } else {
+    indi_funs <- c(indi)
+  }
+
+  # Creates progress bar
+  pb <- progress::progress_bar$new(
+    format = ":spin :current/:total [:bar] :percent in :elapsed ETA: :eta",
+    clear = FALSE, total = length(indi_funs))
+
+  pb$tick(0)
+  Sys.sleep(1)
+
+  # Expand indi_functions and write parquet files
+  for(i in indi_funs){
+    pb$tick(tokens = list(what = i))
+    tmp <- expand_indi(agg = agg, agg_time = agg_time, anos = anos, indi_fun = i)
+    arrow::write_parquet(x = tmp, sink = paste0(dir, "/", as.character(i), ".parquet"))
+  }
+}
---
+++ tests/testthat/test-expand_indi_parquet.R
@@ -0,0 +1,10 @@
+test_that("expand_indi_parquet works", {
+  tmpdir <- tempdir()
+
+  expand_indi_parquet(agg = "uf_res", agg_time = "year", anos = 2010, dir = tmpdir, indi = "indi_0001")
+
+  res <- arrow::read_parquet(paste0(tmpdir, "/", "indi_0001", ".parquet"))
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 20)
+})
