10331d797ef4d8798fac5f3ab1212d634750d5ef
Raphael Saldanha <rfsaldanha@gmail.com>
2023-06-02 11:48:48 +0200

    Adaptations for regsaude 449 codes.

--- tests/testthat/test-indi_0015.R
+++ tests/testthat/test-indi_0015.R
@@ -1,3 +1,5 @@
+# mun res
+
 test_that("indi_0015 works with mun res", {
   res <- indi_0015(agg = "mun_res", ano = 2013)
 
@@ -24,4 +26,64 @@ test_that("indi_0015 works with mun res and multiple years", {
 
   expect_equal("tbl_df", class(res)[1])
   expect_gt(nrow(res), 11100)
+})
+
+# uf res
+
+test_that("indi_0015 works with uf res", {
+  res <- indi_0015(agg = "uf_res", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_equal(nrow(res), 27)
+})
+
+test_that("indi_0015 works with uf res and month aggregation", {
+  res <- indi_0015(agg = "uf_res", agg_time = "month", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 320)
+})
+
+test_that("indi_0015 works with uf res and week aggregation", {
+  res <- indi_0015(agg = "uf_res", agg_time = "week", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 1430)
+})
+
+test_that("indi_0015 works with uf res and multiple years", {
+  res <- indi_0015(agg = "uf_res", ano = c(2013, 2015))
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 50)
+})
+
+# regsaude 449 res
+
+test_that("indi_0015 works with regsaude 449 res", {
+  res <- indi_0015(agg = "regsaude_449_res", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 440)
+})
+
+test_that("indi_0015 works with regsaude 449 res and month aggregation", {
+  res <- indi_0015(agg = "regsaude_449_res", agg_time = "month", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 5300)
+})
+
+test_that("indi_0015 works with regsaude 449 res and week aggregation", {
+  res <- indi_0015(agg = "regsaude_449_res", agg_time = "week", ano = 2013)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 23840)
+})
+
+test_that("indi_0015 works with regsaude 449 res and multiple years", {
+  res <- indi_0015(agg = "regsaude_449_res", ano = c(2013, 2015))
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 890)
 })
