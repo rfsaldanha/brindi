d25f119002fb207a7e8f919b27b1debd9f03b709
Raphael Saldanha <rfsaldanha@gmail.com>
2025-03-19 10:24:26 -0300

    indi_0001 with adjusted rates argument

--- R/indi_0001.R
+++ R/indi_0001.R
@@ -1,14 +1,17 @@
 #' Indicator: Mortalidade por causas externas
 #'
-#' @param agg character. Spatial aggregation level. \code{uf_res} for UF of residence. \code{uf_ocor} for UF of occurrence. \code{regsaude_res} for regiao de saude of residence. \code{regsaude_ocor} for regiao de saude of occurence. \code{regsaude_449_res} for regiao de saude (449 units) of residence. \code{regsaude_449_ocor} for regiao de saude (449 units) of occurence. \code{mun_res} for municipality of residence. \code{mun_ocor} for municipality of ocurrence.
+#' @param agg character. Spatial aggregation level. \code{uf_res} for UF of residence. \code{uf_ocor} for UF of occurrence. \code{regsaude_res} for regiao de saude of residence. \code{regsaude_ocor} for regiao de saúde of occurence. \code{mun_res} for municipality of residence. \code{mun_ocor} for municipality of ocurrence.
 #' @param agg_time character. Time aggregation level. \code{year} for yearly data. \code{month} for monthly data. \code{week} for weekly data. Defaults to \code{year}.
 #' @param ano numeric. Year of death.
 #' @param multi integer. Multiplicator for indicator.
 #' @param decimals integer. Number of decimals for indicator.
-#' @param complete_with_zeros logical. Complete indicator result with zeros considering combinations of spatial and temporal aggregation without results.
-#' @param keep_raw_values logical. Keep numerator and denominator values on results.
-#' @param save_args logical. Save \code{agg} and \code{agg_time} arguments on results table.
+#' @param pop_source character. Population source, from {brpop} package.
+#' @param adjust_rates logical. Adjust rates by age.
 #' @param pcdas_token character. PCDaS API token. If not provided, the function will look for it on renvirom.
+#'
+#' @examples
+#' # Some examples
+#' indi_0001(agg = "mun_res", ano = 2013)
 #'
 #' @importFrom rlang .data
 #' @export
@@ -18,58 +21,69 @@ indi_0001 <- function(
   ano,
   multi = 100000,
   decimals = 2,
-  complete_with_zeros = TRUE,
-  keep_raw_values = FALSE,
-  save_args = FALSE,
-  pcdas_token = NULL
+  pop_source = "datasus",
+  pcdas_token = NULL,
+  adjust_rates = FALSE
 ) {
   # Try to get PCDaS API token from renviron if not provided
   if (is.null(pcdas_token)) {
     pcdas_token <- rpcdas::get_pcdas_token_renviron()
   }
 
-  # Creates numerator
   filter_query <- "LEFT(CAUSABAS, 1) IN ('V', 'W', 'X', 'Y')"
-  numerador <- rpcdas::get_sim(
-    agg = agg,
-    agg_time = agg_time,
-    ano = ano,
-    pcdas_token = pcdas_token,
-    more_filters = filter_query
-  )
 
-  # Creates denominator
-  denominador <- denominator_pop(agg = agg)
-
-  # Perform indicator calculus
-  res <- indicator_raw(
-    numerador = numerador,
-    denominador = denominador,
-    keep_raw_values = keep_raw_values,
-    nome = "indi_0001",
-    agg = agg,
-    multi = multi,
-    decimals = decimals
-  )
-
-  # Save arguments
-  if (save_args == TRUE) {
-    res <- res %>%
-      dplyr::mutate(
-        agg = agg,
-        agg_time = agg_time
-      ) %>%
-      dplyr::relocate("agg", "agg_time", .after = "name")
-  }
-
-  # Complete with zeros
-  if (complete_with_zeros == TRUE) {
-    res <- complete_with_zeros(
-      res = res,
+  if (adjust_rates == FALSE) {
+    # Creates numerator
+    numerador <- rpcdas::get_sim(
       agg = agg,
       agg_time = agg_time,
       ano = ano,
-      save_args = save_args
+      pcdas_token = pcdas_token,
+      more_filters = filter_query
+    )
+
+    # Creates denominator
+    denominador <- denominator_pop(agg = agg, pop_source = pop_source)
+
+    # Perform indicator computation
+    res <- indicator_raw(
+      numerador = numerador,
+      denominador = denominador,
+      denominador_type = "pop",
+      treat_inf_values = TRUE,
+      nome = "indi_0001",
+      ano = ano,
+      agg = agg,
+      agg_time = agg_time,
+      pop_source = pop_source,
+      multi = multi,
+      decimals = decimals
+    )
+  } else if (adjust_rates == TRUE) {
+    # Prepate multission environment
+    oplan <- future::plan(future::multisession)
+    on.exit(future::plan(oplan))
+
+    # Creates numerator
+    numerador <- furrr::future_pmap(
+      .l = age_groups,
+      .f = rpcdas::get_sim,
+      agg = agg,
+      agg_time = agg_time,
+      ano = ano,
+      more_filters = filter_query
+    )
+
+    # Age adjusted indicator computation
+    res <- indicator_adjusted(
+      numerador = numerador,
+      ano = ano,
+      agg = agg,
+      agg_time = agg_time,
+      pop_source = pop_source,
+      nome = "indi_0001",
+      multi = multi,
+      decimals = decimals
     )
   }
 
