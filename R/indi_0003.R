59f1ece5b737391d00f2ac1933d351026a495889
Raphael Saldanha <rfsaldanha@gmail.com>
2025-03-19 11:15:33 -0300

    Adjusted rates

--- R/indi_0003.R
+++ R/indi_0003.R
@@ -5,58 +5,86 @@
 #' @param ano numeric. Year of death.
 #' @param multi integer. Multiplicator for indicator.
 #' @param decimals integer. Number of decimals for indicator.
-#' @param complete_with_zeros logical. Complete indicator result with zeros considering combinations of spatial and temporal aggregation without results.
-#' @param keep_raw_values logical. Keep numerator and denominator values on results.
-#' @param save_args logical. Save \code{agg} and \code{agg_time} arguments on results table.
+#' @param pop_source character. Population source, from {brpop} package.
+#' @param adjust_rates logical. Adjust rates by age.
 #' @param pcdas_token character. PCDaS API token. If not provided, the function will look for it on renvirom.
+#'
+#' @examples
+#' # Some examples
+#' indi_0003(agg = "mun_res", ano = 2013)
 #'
 #' @importFrom rlang .data
 #' @export
-indi_0003 <- function(agg, agg_time = "year", ano, multi = 100000, decimals = 2, complete_with_zeros = TRUE, keep_raw_values = FALSE, save_args = FALSE, pcdas_token = NULL){
-
+indi_0003 <- function(
+  agg,
+  agg_time = "year",
+  ano,
+  multi = 100000,
+  decimals = 2,
+  pop_source = "datasus",
+  pcdas_token = NULL,
+  adjust_rates = FALSE
+) {
   # Try to get PCDaS API token from renviron if not provided
-  if(is.null(pcdas_token)){
+  if (is.null(pcdas_token)) {
     pcdas_token <- rpcdas::get_pcdas_token_renviron()
   }
 
-  # Creates numerator
   filter_query <- "LEFT(CAUSABAS, 3) IN ('I20', 'I21', 'I22', 'I23', 'I24', 'I25')"
-  numerador <- rpcdas::get_sim(
-    agg = agg,
-    agg_time = agg_time,
-    ano = ano,
-    pcdas_token = pcdas_token,
-    more_filters = filter_query
-  )
 
-  # Creates denominator
-  denominador <- denominator_pop(agg = agg)
+  if (adjust_rates == FALSE) {
+    # Creates numerator
+    numerador <- rpcdas::get_sim(
+      agg = agg,
+      agg_time = agg_time,
+      ano = ano,
+      pcdas_token = pcdas_token,
+      more_filters = filter_query
+    )
 
-  # Perform indicator calculus
-  res <- indicator_raw(
-    numerador = numerador,
-    denominador = denominador,
-    multi = multi,
-    decimals = decimals,
-    keep_raw_values = keep_raw_values,
-    nome = "indi_0003",
-    agg = agg
-  )
+    # Creates denominator
+    denominador <- denominator_pop(agg = agg, pop_source = pop_source)
 
-  # Save arguments
-  if(save_args == TRUE){
-    res <- res %>%
-      dplyr::mutate(
-        agg = agg,
-        agg_time = agg_time
-      ) %>%
-      dplyr::relocate("agg", "agg_time", .after = "name")
-  }
+    # Perform indicator computation
+    res <- indicator_raw(
+      numerador = numerador,
+      denominador = denominador,
+      denominador_type = "pop",
+      treat_inf_values = TRUE,
+      nome = "indi_0003",
+      ano = ano,
+      agg = agg,
+      agg_time = agg_time,
+      pop_source = pop_source,
+      multi = multi,
+      decimals = decimals
+    )
+  } else if (adjust_rates == TRUE) {
+    # Prepate multission environment
+    oplan <- future::plan(future::multisession)
+    on.exit(future::plan(oplan))
 
-  # Complete with zeros
-  if(complete_with_zeros == TRUE){
-    res <- complete_with_zeros(res = res, agg = agg, agg_time = agg_time,
-                               ano = ano, save_args = save_args)
+    # Creates numerator
+    numerador <- furrr::future_pmap(
+      .l = age_groups,
+      .f = rpcdas::get_sim,
+      agg = agg,
+      agg_time = agg_time,
+      ano = ano,
+      more_filters = filter_query
+    )
+
+    # Age adjusted indicator computation
+    res <- indicator_adjusted(
+      numerador = numerador,
+      ano = ano,
+      agg = agg,
+      agg_time = agg_time,
+      pop_source = pop_source,
+      nome = "indi_0003",
+      multi = multi,
+      decimals = decimals
+    )
   }
 
   return(res)
