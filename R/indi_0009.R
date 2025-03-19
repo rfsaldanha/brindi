6c9c963fd7234af5035af2cffcb5076a5e96328e
Raphael Saldanha <rfsaldanha@gmail.com>
2025-03-19 15:18:46 -0300

    Update function

--- R/indi_0009.R
+++ R/indi_0009.R
@@ -5,10 +5,13 @@
 #' @param ano numeric. Year of death.
 #' @param multi integer. Multiplicator for indicator.
 #' @param decimals integer. Number of decimals for indicator.
-#' @param complete_with_zeros logical. Complete indicator result with zeros considering combinations of spatial and temporal aggregation without results.
-#' @param keep_raw_values logical. Keep numerator and denominator values on results.
-#' @param save_args logical. Save \code{agg} and \code{agg_time} arguments on results table.
+#' @param pop_source character. Population source, from {brpop} package.
+#' @param adjust_rates logical. Adjust rates by age. Argument ignored for this indicator.
 #' @param pcdas_token character. PCDaS API token. If not provided, the function will look for it on renvirom.
+#'
+#' @examples
+#' # Some examples
+#' indi_0009(agg = "mun_res", ano = 2013)
 #'
 #' @importFrom rlang .data
 #' @export
@@ -18,10 +21,9 @@ indi_0009 <- function(
   ano,
   multi = 100,
   decimals = 2,
-  complete_with_zeros = TRUE,
-  keep_raw_values = FALSE,
-  save_args = FALSE,
-  pcdas_token = NULL
+  pop_source = "datasus",
+  pcdas_token = NULL,
+  adjust_rates = FALSE
 ) {
   # Try to get PCDaS API token from renviron if not provided
   if (is.null(pcdas_token)) {
@@ -49,38 +51,19 @@ indi_0009 <- function(
     pcdas_token = pcdas_token
   )
 
-  # Perform indicator calculus
+  # Perform indicator computation
   res <- indicator_raw(
     numerador = numerador,
     denominador = denominador,
     denominador_type = "arbitrary",
     multi = multi,
     decimals = decimals,
-    keep_raw_values = keep_raw_values,
     nome = "indi_0009",
-    agg = agg
+    ano = ano,
+    agg = agg,
+    agg_time = agg_time,
+    pop_source = pop_source
   )
-
-  # Save arguments
-  if (save_args == TRUE) {
-    res <- res %>%
-      dplyr::mutate(
-        agg = agg,
-        agg_time = agg_time
-      ) %>%
-      dplyr::relocate("agg", "agg_time", .after = "name")
-  }
-
-  # Complete with zeros
-  if (complete_with_zeros == TRUE) {
-    res <- complete_with_zeros(
-      res = res,
-      agg = agg,
-      agg_time = agg_time,
-      ano = ano,
-      save_args = save_args
-    )
-  }
 
   return(res)
 }
