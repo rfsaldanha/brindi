a2a0f1e1a96a53910abce2f6b995a7b6801de903
Raphael Saldanha <rfsaldanha@gmail.com>
2025-03-19 15:29:02 -0300

    Adjusted rates

--- R/indi_0010.R
+++ R/indi_0010.R
@@ -5,22 +5,32 @@
 #' @param ano numeric. Year of death.
 #' @param multi integer. Multiplicator for indicator.
 #' @param decimals integer. Number of decimals for indicator.
-#' @param complete_with_zeros logical. Complete indicator result with zeros considering combinations of spatial and temporal aggregation without results.
-#' @param keep_raw_values logical. Keep numerator and denominator values on results.
-#' @param save_args logical. Save \code{agg} and \code{agg_time} arguments on results table.
+#' @param pop_source character. Population source, from {brpop} package.
+#' @param adjust_rates logical. Adjust rates by age. Argument ignored for this indicator.
 #' @param pcdas_token character. PCDaS API token. If not provided, the function will look for it on renvirom.
+#'
+#' @examples
+#' # Some examples
+#' indi_0010(agg = "mun_res", ano = 2013)
 #'
 #' @importFrom rlang .data
 #' @export
-indi_0010 <- function(agg, agg_time = "year", ano, multi = 1000, decimals = 2, complete_with_zeros = TRUE, keep_raw_values = FALSE, save_args = FALSE, pcdas_token = NULL){
-
+indi_0010 <- function(
+  agg,
+  agg_time = "year",
+  ano,
+  multi = 1000,
+  decimals = 2,
+  pop_source = "datasus",
+  pcdas_token = NULL,
+  adjust_rates = FALSE
+) {
   # Try to get PCDaS API token from renviron if not provided
-  if(is.null(pcdas_token)){
+  if (is.null(pcdas_token)) {
     pcdas_token <- rpcdas::get_pcdas_token_renviron()
   }
 
   # Creates numerator
-
   numerador <- rpcdas::get_sim(
     agg = agg,
     agg_time = agg_time,
@@ -45,27 +55,12 @@ indi_0010 <- function(agg, agg_time = "year", ano, multi = 1000, decimals = 2, c
     denominador_type = "arbitrary",
     multi = multi,
     decimals = decimals,
-    keep_raw_values = keep_raw_values,
     nome = "indi_0010",
-    agg = agg
+    ano = ano,
+    agg = agg,
+    agg_time = agg_time,
+    pop_source = pop_source
   )
-
-  # Save arguments
-  if(save_args == TRUE){
-    res <- res %>%
-      dplyr::mutate(
-        agg = agg,
-        agg_time = agg_time
-      ) %>%
-      dplyr::relocate("agg", "agg_time", .after = "name")
-  }
-
-  # Complete with zeros
-  if(complete_with_zeros == TRUE){
-    res <- complete_with_zeros(res = res, agg = agg, agg_time = agg_time,
-                               ano = ano, save_args = save_args)
-  }
 
   return(res)
 }
-
